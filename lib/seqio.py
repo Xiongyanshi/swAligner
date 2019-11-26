31228ab064217995f188d864c14f30dd9beb6925
Xiongyanshi <1240172230@qq.com>
2019-11-26 22:16:24 +0800

    20191126 seqio module not designed well

--- lib/seqio.py
+++ lib/seqio.py
@@ -1,12 +1,21 @@
 import re
 
-class Seq:
-    def __init__(self, filename):
-        self.__filecontent = open(filename, 'r').read()
+class FastaIO:
+    def __init__(self):
+        pass
+
+    def parse_String(self, stringContent):
+        """parse sequence file, get two reads"""
         try:
-            name1, seq1, name2, seq2 = re.match(r'^>(.+?)\n([\w\W]+?)\n>(.+?)\n([\w\W]+)\n>.+', self.__filecontent).groups()
+            # more than 2 > in fasta file?
+            name1, seq1, name2, seq2 = re.match(
+                                 r'^>(.+?)\n([\w\W]+?)\n>(.+?)\n([\w\W]+)\n>.+',
+                                 stringContent).groups()
         except AttributeError:
-            name1, seq1, name2, seq2 = re.match(r'^>(.+?)\n([\w\W]+?)\n>(.+?)\n([\w\W]+)', self.__filecontent).groups()
+            # no just 2 > records in fasta file, safe to parse until file end
+            name1, seq1, name2, seq2 = re.match(
+                                 r'^>(.+?)\n([\w\W]+?)\n>(.+?)\n([\w\W]+)',
+                                 stringContent).groups()
         seq1 = ''.join(seq1.splitlines())
         seq2 = ''.join(seq2.splitlines())
 
@@ -18,7 +27,13 @@ class Seq:
         self.read1 = '>%s\n%s' % (name1, seq1)
         self.read2 = '>%s\n%s' % (name2, seq2)
 
-    def write(self, outfilename):
+        return self
+
+    def parse_File(self, filename):
+        return FastaIO.parse_String(open(filename).read())
+
+    def to_file(self, outfilename):
         with open(outfilename, 'w') as handle:
-            handle.write('>%s\n%s\n>%s\n%s' % (self.name1, self.seq1, self.name2, self.seq2))
+            handle.write('>%s\n%s\n>%s\n%s' % (self.name1, self.seq1,
+                                               self.name2, self.seq2))
 
--- swAligner.py
+++ swAligner.py
@@ -1,13 +1,23 @@
 import argparse
-from seqio import Seq
+from lib.seqio import Seq
 
 def main():
     parser = argparse.ArgumentParser(description='Smith-Waterman pairwise alignment (toy) by Yanshi Xiong')
-    parser.add_argument('-i','--input', required=True, help='file name of input fasta for alignment, only first 2 sequences will be read in.')
-    parser.add_argument('-o','--output', required=True, help='file name of alignment result in fasta format')
+    parser.add_argument('-i','--input', required=True,
+                        help='file name of input fasta for alignment, only first 2 sequences will be read in.')
+    parser.add_argument('-o','--output', required=True,
+                        help='file name of alignment result in fasta format')
 
     args = parser.parse_args()
+    inputfile = args['input']
+    outfile = args['output']
+
+    name1, seq1, name2, seq2 = Seq(inputfile)
     print(args)
+    print(name1)
+    print(seq1)
+    print(name2)
+    print(seq2)
 
 if __name__ == '__main__':
     main()
