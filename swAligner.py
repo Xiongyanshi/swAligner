bdcca1575936abda19d2f003766c0e7a15b4117f
ysh <1240172230@qq.com>
2019-12-03 17:08:30 +0800

    my code seems working, but I can't figure out why

--- swAligner.py
+++ swAligner.py
@@ -18,19 +18,21 @@ def tailn(l, x):
     tail([0,1,2,3,4,3,3,3,3], 3) = 4
     """
     count = 0
-    for i in list(l)[::-1]:
+    for i in l[::-1]:
         if i != x:
             break
         count += 1
     return count
 
 class Align:
-    def __init__(self, seq1, seq2, submat, gap_open, gap_extend):
+    def __init__(self, seq1, seq2, submat, gap_open, gap_extend, algo):
         self.seq1 = '-' + seq1.upper()
         self.seq2 = '-' + seq2.upper()
         self.submat = submat
         self.gap_open = gap_open
         self.gap_extend = gap_extend
+        self.algo = algo
+
         Align.__buildmat(self)
         Align.__traceback(self)
         Align.__makeprintable(self)
@@ -40,72 +42,97 @@ class Align:
                                  dtype=np.int32)
         self.tracemat = np.empty([len(self.seq1), len(self.seq2)],
                                  dtype=np.int8)
+
         for i, p in enumerate(self.seq1):
             for j, q in enumerate(self.seq2):
                 if i == 0:
-                    self.scoremat[i, j] = 0
-                    self.tracemat[i, j] = 1
+                    if self.algo == 'sw':
+                        self.scoremat[i,j] = 0
+                    elif self.algo == 'nw':
+                        self.scoremat[i,j] = j*self.gap_extend + self.gap_open
+                    self.tracemat[i,j] = 1
                     continue
                 if j == 0:
-                    self.scoremat[i, j] = 0
-                    self.tracemat[i, j] = 2
+                    if self.algo == 'sw':
+                        self.scoremat[i,j] = 0
+                    elif self.algo == 'nw':
+                        self.scoremat[i,j] = i*self.gap_extend + self.gap_open
+                    self.tracemat[i,j] = 2
                     continue
 
                 # from diagnoal
                 d = self.scoremat[i-1, j-1] + self.submat[p][q]
 
                 # from left side
-                lgaplen = tailn(self.tracemat[i,:j], 1)
-                lgap_penalty = lgaplen * self.gap_open + self.gap_extend
+                lgaplen = tailn(self.tracemat[i,:j], 1) + 1   # +1 for itself
+                lgap_penalty = lgaplen * self.gap_extend + self.gap_open
                 l = self.scoremat[i, j-1] + lgap_penalty + self.submat[p][q]
 
                 # from up side
-                ugaplen = tailn(self.tracemat[:i,j], 2)
-                ugap_penalty = ugaplen * self.gap_open + self.gap_extend
+                ugaplen = tailn(self.tracemat[:i,j], 2) + 1   # +1 for itself
+                ugap_penalty = ugaplen * self.gap_extend + self.gap_open
                 u = self.scoremat[i-1, j] + ugap_penalty + self.submat[p][q]
 
-                self.scoremat[i, j] = max(d,l,u,0)
-                self.tracemat[i, j] = [d, l, u, 0].index(max(d,l,u,0))
+                if self.algo == 'sw':
+                    self.scoremat[i, j] = max(d,l,u,0)
+                    self.tracemat[i, j] = [d, l, u, 0].index(max(d,l,u,0))
+                elif self.algo == 'nw':
+                    self.scoremat[i, j] = max(d,l,u)
+                    self.tracemat[i, j] = [d, l, u].index(max(d,l,u))
+
+        print(self.scoremat)
+        print()
+        #print(self.tracemat)
 
     def __traceback(self):
         self.maxscore = self.scoremat.max()
         self.maxscorei = np.unravel_index(self.scoremat.argmax(),
-                                          self.scoremat.shape)
+                                               self.scoremat.shape)
+        def reachEnd(self, i, j):
+            if self.algo == 'sw':
+                return self.scoremat[i,j] == 0     # 0-scored cell is end.
+            else:
+                return (i,j) == (0,0)              # [0, 0] cell is end.
+
+        if self.algo == 'sw':
+            self.end = self.maxscorei              # start loc of tracing back
+        elif self.algo == 'nw':
+            self.end = len(self.seq1)-1, len(self.seq2)-1
+
         self.pathcode = ''
-        i, j = self.maxscorei   # this is where we start to traceback
-        while self.scoremat[i,j] != 0:
-            #print('[%s,%s]>' % (i, j), end='')
+        i, j = self.end
+        while not reachEnd(self, i, j):
+            print('[%s,%s]> ' % (i, j), end='')
             direction = str(self.tracemat[i,j])
             self.pathcode = direction + self.pathcode
             if direction == '0':
                 i -= 1
                 j -= 1
-            if direction == '1':
+            elif direction == '1':
                 j -= 1
-            if direction == '2':
+            elif direction == '2':
                 i -= 1
-            if direction == '3':   # this is not in plan.
-                print('wrong')
-                print(self.scoremat)
-                print(self.tracemat)
-                print(i, j)
-        self.start = [i,j]
-        self.end = self.maxscorei
+        self.start = [i,j]         # this is the end of tracing back journey,
+                                   # yet start of alignment.
+
+        print(self.start)
+        #print()
+        #print('%s -> %s' % (self.start, self.end))
+        print(self.pathcode)
 
     def __makeprintable(self):
-        pathcode = self.pathcode
         i, j = self.start  # start of aligned base
         i = i+1
         j = j+1
 
-        # the unaligned seq at left side
+        # unaligned sequence at left side
         leftsize = max(i, j) - 1                   # -1 : cut '-' place
         top    = '{:>{leftsize}}'.format(self.seq1[1:i], leftsize=leftsize)
         middle = ' ' * leftsize
         bottom = '{:>{leftsize}}'.format(self.seq2[1:j], leftsize=leftsize)
 
-        # connect with aligned body
-        for path in pathcode:
+        # aligned body
+        for path in self.pathcode:
             if path == '0':
                 top    += self.seq1[i]
                 middle += '|' if self.seq1[i] == self.seq2[j] else '*'
@@ -123,7 +150,7 @@ class Align:
                 bottom += '-'
                 i += 1
 
-        # tail with unaligned sequence at right side
+        # unaligned sequence at right side
         m, n = self.end
         m += 1
         n += 1
@@ -164,6 +191,10 @@ def main():
     parser.add_argument('-r', '--reads', required=False,
                         default=None,
                         help='two reads seperated by "," (skip open file)')
+    parser.add_argument('-a', '--algo', required=False,
+                        choices=['sw','nw'], default='sw',
+                        help='''Smith-Waterman (local, default) or
+                                Needleman-Wunsch (global) algorithm''')
     parser.add_argument('-m','--submat', required=False,
                         default='./lib/dna_sub.default.mat',
                         help='file name of base substitution matrix')
@@ -177,6 +208,7 @@ def main():
     submat     = readmat(args['submat'])
     gap_open   = readgap(args['gap'])['gap_open']
     gap_extend = readgap(args['gap'])['gap_extend']
+    algo       = args['algo']
 
     # get input
     if args['reads']:
@@ -186,7 +218,7 @@ def main():
         seq1, seq2 = infasta.seq1, infasta.seq2
 
     # ready to go
-    align = Align(seq1, seq2, submat, gap_open, gap_extend)
+    align = Align(seq1, seq2, submat, gap_open, gap_extend, algo)
     align.print_align()
     #align.print_scoremat()
     #align.print_tracemat()
